a701a699fe66d92efd1df6e8e4655e454e812031
João Ângelo <134436082+joaoangelo76@users.noreply.github.com>
2024-03-28 17:30:21 -0300

    Commit atividade3

---
+++ atividade3/matrizTransposta.js
@@ -0,0 +1,11 @@
+matriz = [[1,2,3],[4,5,6],[7,8,9]];
+
+function transporMatriz(A){
+    for(let lin=0;lin<3;l++){
+        for(let col=0;col<3;col++){
+           console.log()
+        }
+    }
+}
+
+transporMatriz(matriz = []);
